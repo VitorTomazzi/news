84684f4420a71f93d3c4abeaa262805347e27beb
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-11-12 21:24:50 -0500

    news start

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+//import {Text, View} from 'react-native';
+import TabScreen from './src/screens/TabScreen';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return <TabScreen />;
+  }
+}
