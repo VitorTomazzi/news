3bd1f53523b34637e1c5d66411df07e2b082fc96
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-11-22 16:15:37 -0500

    hiding

--- src/config/rest_config.js
+++
@@ -1,4 +0,0 @@
-export const articles_url = 'https://newsapi.org/v2/top-headlines';
-export const country_code = 'us';
-export const category = 'general';
-export const API_KEY = '80d5ff3c23044c179d03e1dbe00190a9';
